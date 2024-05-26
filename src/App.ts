0333a4adcb081a2423b687c1b6f92855a4f56ee7
chenAmrani <chenamrani5@gmail.com>
2024-05-26 22:41:33 +0300

    update

--- src/App.ts
+++ src/App.ts
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
+
+
+
 const donorRoute = require('./routes/donor_route');
 const adminRoute = require('./routes/admin_route');
 const donationRoute = require('./routes/donation_route');
@@ -21,8 +24,9 @@ mongoose.connection.once("open", () => { console.log("connected to DB") });
 const app = express();
 const session = require('express-session');
 app.use(session({
-    saveUninitialized: false,
-    resave: false
+    secret: "fbb866cf21c7d5b59f4598a9157171af6f9769806d4e0590ade0da2d5ee76c41e0abbd92082248c573f088f3647f4707c4389526adf1a07e14b9e684aad6423b",
+    resave: false,
+    saveUninitialized: true
 }));
 
 app.use(express.static('public'));
@@ -37,5 +41,6 @@ app.use('/donation', donationRoute);
 app.use('/auth', authRoute);
 app.use('/file', fileRoute);
 app.use('/profile', profileRoute);
+
 
 export default app;
