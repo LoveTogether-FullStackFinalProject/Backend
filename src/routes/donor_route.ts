8011cbb8e372f456330766822599f800909ea98c
Nadav levy <69619783+nadavlevy40@users.noreply.github.com>
2024-05-31 15:07:45 +0300

    removed profile controller  and route

--- src/routes/donor_route.ts
+++ src/routes/donor_route.ts
@@ -1,22 +1,22 @@
 import express from 'express';
-import DonorController from '../controllers/donor_controller'; 
+import DonorController from '../controllers/donor_controller';
 import authMiddleware from '../common/auth_middelware';
 
 const router = express.Router();
 
 // Route for getting all donors
-router.get('/',authMiddleware, DonorController.get);
+router.get('/', DonorController.get);
 
 // Route for getting a single donor by ID
-router.get('/:id',authMiddleware, DonorController.getById);
+router.get('/:id', DonorController.getById);
 
 // Route for creating a new donor
-router.post('/',authMiddleware, DonorController.post);
+router.post('/', DonorController.post);
 
 // Route for updating a donor by ID
-router.put('/:id',authMiddleware, DonorController.putById);
+router.put('/:id', DonorController.putById);
 
 // Route for deleting a donor by ID
-router.delete('/:id',authMiddleware, DonorController.deleteById);
+router.delete('/:id', DonorController.deleteById);
 
 export default router;
