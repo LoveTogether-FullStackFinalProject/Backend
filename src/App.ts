dfda95c970e4bd9a03b8cc8fd059d4f8eb52c080
Nadav levy <69619783+nadavlevy40@users.noreply.github.com>
2024-06-05 20:35:19 +0300

    Revert " .."
    This reverts commit f16b1f960661f148a36729cd7cdaf051b76e00c3.

--- src/App.ts
+++ src/App.ts
@@ -1,84 +1,3 @@
-// import env from "dotenv";
-// env.config();
-// import express, { Express } from "express";
-// import mongoose from "mongoose";
-// import bodyParser from "body-parser";
-// import AdminRoute from "./routes/admin_route";
-// import AuthRoute from "./routes/auth_route";
-// import DonationRoute from "./routes/donation_route";
-// import DonorRoute from "./routes/donor_route";
-// import FileRoute from "./routes/file_route";
-// import ProfileRoute from "./routes/profile_route";
-// import swaggerUI from "swagger-ui-express"
-// import swaggerJsDoc from "swagger-jsdoc"
-// import cors from "cors";
-
-// const initApp = (): Promise<Express> => {
-//   const promise = new Promise<Express>((resolve) => {
-//     const db = mongoose.connection;
-//     db.once("open", () => console.log("Connected to Database"));
-//     db.on("error", (error) => console.error(error));
-//     const url = process.env.DB_URL;
-//     mongoose.connect(url!).then(() => {
-//       const app = express();
-//       //app.use(cors());
-//       //app.use(cors({ origin: 'http://localhost:5173' }));
-//      //app.use(cors({ origin: '*' }));
-//       app.use(bodyParser.json());
-//       app.use(bodyParser.urlencoded({ extended: true }));
-//       app.use((req, res, next) => {
-//         res.setHeader('Access-Control-Allow-Origin','');
-//         res.setHeader('Access-Control-Allow-Methods', '');
-//         res.setHeader('Access-Control-Allow-Headers', '');
-//         next();
-//       });
-//       app.use((req, res, next) => {
-//         // res.header("Access-Control-Allow-Origin", "*");
-//         // res.header("Access-Control-Allow-Methods", "*");
-//         // res.header("Access-Control-Allow-Headers", "*");
-//         next();
-//       })
-//       // app.use((req, res, next) => {
-//       //   res.header("Access-Control-Allow-Origin", "*");
-//       //   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-//       //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//       //   next();
-//       // })
-//       const options = {
-//         definition: {
-//           openapi: "3.0.0",
-//           info: {
-//             title: "Vehahavtem Together 2024",
-//             version: "1.0.1",
-//             description: "Full Stack Project 2024",
-//           },
-//         //   servers: [{ url: "https://node12.cs.colman.ac.il/", },],
-//         },
-//         apis: ["./src/routes/.ts"],
-//       };
-//       const specs = swaggerJsDoc(options);
-//       app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
-
-//       app.use("/admin", AdminRoute);
-//       app.use("/auth", AuthRoute);
-//       app.use("/donation", DonationRoute);
-//       app.use("/donor", DonorRoute);
-//       app.use("/file", FileRoute);
-//       app.use("/profile", ProfileRoute);
-//       app.use("/public", express.static("public"));
-//     //   app.use(express.static('dist/client'))
-//     //   app.get('*',function (req, res) {
-//     //     res.sendfile('dist/client/index.html')
-//     //   });
-//        resolve(app);
-//     });
-//   });
-//   return promise;
-// };
-
-// export default initApp;
-
-
 import env from "dotenv";
 env.config();
 import express, { Express } from "express";
@@ -102,10 +21,21 @@ const initApp = (): Promise<Express> => {
     const url = process.env.DB_URL;
     mongoose.connect(url!).then(() => {
       const app = express();
-      app.use(cors());
+      //app.use(cors());
+      //app.use(cors({ origin: 'http://localhost:5173' }));
+     //app.use(cors({ origin: '*' }));
       app.use(bodyParser.json());
       app.use(bodyParser.urlencoded({ extended: true }));
+      // app.use((req, res, next) => {
+      //   res.setHeader('Access-Control-Allow-Origin', '*');
+      //   res.setHeader('Access-Control-Allow-Methods', '*');
+      //   res.setHeader('Access-Control-Allow-Headers', '*');
+      //   next();
+      // });
       app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "*");
+        res.header("Access-Control-Allow-Headers", "*");
         next();
       })
       // app.use((req, res, next) => {
@@ -122,11 +52,11 @@ const initApp = (): Promise<Express> => {
             version: "1.0.1",
             description: "Full Stack Project 2024",
           },
+        //   servers: [{ url: "https://node12.cs.colman.ac.il/", },],
         },
         apis: ["./src/routes/.ts"],
       };
       const specs = swaggerJsDoc(options);
-
       app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
       app.use("/admin", AdminRoute);
@@ -137,6 +67,10 @@ const initApp = (): Promise<Express> => {
       app.use("/requestedDonation", requestedDonationRoute);
       
       app.use("/public", express.static("public"));
+    //   app.use(express.static('dist/client'))
+    //   app.get('*',function (req, res) {
+    //     res.sendfile('dist/client/index.html')
+    //   });
        resolve(app);
     });
   });
