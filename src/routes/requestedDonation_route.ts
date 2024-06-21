148a7c53074caefa9ff4caff8e1c34a21f4f058e
Michal Massachi <michal2792000@gmail.com>
2024-06-21 19:46:28 +0300

    adding authMiddleware to requested product route

--- src/routes/requestedDonation_route.ts
+++ src/routes/requestedDonation_route.ts
@@ -1,14 +1,15 @@
 import express from "express";
 import requestedDonationController from "../controllers/requestedDonation_controller";
 const router = express.Router();
+import authMiddleware from "../common/auth_middelware";
 
 
 
 router.get("/rdonations",requestedDonationController.getAllrequestedDonations);
 router.get("/rdonation/:id",requestedDonationController.getRequestedDonationById);
-router.post("/rdonation",requestedDonationController.createRequestedDonation);
-router.put("/rdonation/update/:id",requestedDonationController.updateRequestedDonation);
-router.delete("/rdonation/delete/:id",requestedDonationController.deleteRequestedDonation);
+router.post("/rdonation",authMiddleware,requestedDonationController.createRequestedDonation);
+router.put("/rdonation/update/:id",authMiddleware,requestedDonationController.updateRequestedDonation);
+router.delete("/rdonation/delete/:id", authMiddleware,requestedDonationController.deleteRequestedDonation);
 
 
 export default router;
