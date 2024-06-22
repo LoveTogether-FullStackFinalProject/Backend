c8328f1dabff01dcee1a77c1a04878dec25d20e6
Michal Massachi <michal2792000@gmail.com>
2024-06-22 15:48:06 +0300

    adding the server and routes to swagger in app.ts

--- src/routes/auth_route.ts
+++ src/routes/auth_route.ts
@@ -2,6 +2,128 @@ import express from "express";
 const router = express.Router();
 import authController from "../controllers/auth_controller";
 
+/**
+* @swagger
+* tags:
+*   name: Auth
+*   description: The Authentication API
+*/
+
+/**
+* @swagger
+* components:
+*   securitySchemes:
+*     bearerAuth:
+*       type: http
+*       scheme: bearer
+*       bearerFormat: JWT
+*/
+
+/**
+* @swagger
+* components:
+*   schemas:
+*     User:
+*       type: object
+*       required:
+*         - firstName
+*         - lastName
+*         - email
+*         - password
+*         - mainAddress
+*         - image
+*       properties:
+*         firstName:
+*           type: string
+*           description: The user's first name.
+*         lastName:
+*           type: string
+*           description: The user's last name.
+*         _id:
+*           type: string
+*           description: The user's unique ID.
+*         image:
+*           type: string
+*           description: The user's image URL.
+*         email:
+*           type: string
+*           description: The user's email address.
+*         password:
+*           type: string
+*           description: The user's password.
+*         mainAddress
+*           type: string
+*           description: The user's main address.
+*         refreshTokens:
+*           type: array
+*           items:
+*             type: string
+*           description: The user's refresh tokens.
+*       example:
+*         first Name: "John"
+*         last Name: "Doe"
+*         _id: "60d725b057d6d8c8c8febe8a"
+*         image: "http://example.com/image.jpg"
+*         email: "johndoe@example.com"
+*         password: "mySecurePassword"
+*         mainAddress: "123 Main St, Springfield, IL 62701"
+*         refreshTokens: ["token1", "token2"]
+*/
+
+/**
+* @swagger
+* /auth/register:
+*   post:
+*     summary: Register a new user
+*     tags: [Auth]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             $ref: '#/components/schemas/User'
+*     responses:
+*       200:
+*         description: The new user
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/User'
+*       400:
+*         description: Bad Request
+*         content:
+*           application/json:
+*             schema:
+*               type: object
+*               properties:
+*                 message: 
+*                   type: string
+*               example:
+*                 message: "Invalid input data"
+*       401:
+*         description: Unauthorized
+*         content:
+*           application/json:
+*             schema:
+*               type: object
+*               properties:
+*                 message: 
+*                   type: string
+*               example:
+*                 message: "Unauthorized access"
+*       406:
+*         description: Not Acceptable
+*         content:
+*           application/json:
+*             schema:
+*               type: object
+*               properties:
+*                 message: 
+*                   type: string
+*               example:
+*                 message: "Request not acceptable"
+*/
+
 router.post("/register", authController.register);
 
 router.post("/googleSignIn", authController.googleSignin);
--- src/routes/file_route.ts
+++ src/routes/file_route.ts
@@ -17,6 +17,71 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
+/**
+* @swagger
+* tags:
+*   name: Files
+*   description: The Files API
+*/
+
+/**
+* @swagger
+* components:
+*   securitySchemes:
+*     bearerAuth:
+*       type: http
+*       scheme: bearer
+*       bearerFormat: JWT
+*/
+
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ *   schemas:
+ *     FileUploadResponse:
+ *       type: object
+ *       required:
+ *         - url
+ *       properties:
+ *         url:
+ *           type: string
+ *           description: The URL of the uploaded file
+ *       example:
+ *        url: "http://localhost:3000/public/1625760000000.jpg"
+ * */
+
+ /**
+ * @swagger
+ * /file:
+ *   post:
+ *     summary: Upload a file
+ *     tags: [Files]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               file:
+ *                 type: string
+ *                 format: binary
+ *     responses:
+ *       200:
+ *         description: The URL of the uploaded file
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/FileUploadResponse'
+ */
+
 router.post('/upload', upload.single("file"), function (req, res) { 
     if (req.file) {
         console.log("router.post(/file: " + base + req.file.path);
