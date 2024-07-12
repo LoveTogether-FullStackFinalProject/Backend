2086ae1cd5178682ff29177265f3e3fc53d02f64
Michal Massachi <michal2792000@gmail.com>
2024-07-12 13:26:56 +0300

    adding authMiddleware to routes

--- src/routes/donation_route.ts
+++ src/routes/donation_route.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import DonationController from '../controllers/donation_controller';
+import authMiddleware from "../common/auth_middelware";
 
 const router = express.Router();
 
 router.get('/donations', DonationController.getAllDonations);
 router.get('/donation/:id', DonationController.getDonationById);
-router.put('/update/:id', DonationController.updateDonation);
-router.delete('/delete/:id', DonationController.deleteDonation);
-router.post('/upload', DonationController.uploadDonation);
+router.put('/update/:id',authMiddleware,  DonationController.updateDonation);
+router.delete('/delete/:id',authMiddleware,  DonationController.deleteDonation);
+router.post('/upload',authMiddleware, DonationController.uploadDonation);
 router.get('/user/:userId', DonationController.getDonationsByUserId);
 
 export default router;
